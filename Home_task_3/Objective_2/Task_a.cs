670643b0d8e64a8c6ca47aff9a0312ca05a5359c
Rostyk <rostyk.gydzelyak@gmail.com>
2023-04-03 00:00:37 +0300

    Fixing bug

--- Home_task_3/Objective_2/Task_a.cs
+++ Home_task_3/Objective_2/Task_a.cs
@@ -10,6 +10,7 @@ namespace Objective_2
     {                                       // входження існує, у іншому випадку - повернути null.
         private string _userString;
         private string _stringToFind;
+        
         public Task_a(string userString, string stringToFind)
         {
             _userString = userString;
@@ -17,7 +18,19 @@ namespace Objective_2
         }
         public override string ToString()
         {
+            int n = 0, counter = 0;
             int? index = _userString.IndexOf(_stringToFind);
+
+            while (n != -1)
+            {
+                n = _userString.IndexOf(_stringToFind, n + _stringToFind.Length);
+                counter++;
+                if (counter == 2)
+                {
+                    index = _userString.IndexOf(_stringToFind, n);
+                    break;
+                }          
+            }          
             if (index == -1)
             {
                 index = null;
