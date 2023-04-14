0b589969d5d2f2e1ca371fc2536c0232d18312ec
Rostyk <rostyk.gydzelyak@gmail.com>
2023-04-14 21:12:05 +0300

    Home_task_4-Objective_1

---
+++ Home_task_4/Objective_1/BracketString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objective_1
+{
+    internal class BracketString
+    {
+        private string _text = "";
+        string[] _sentences;
+        List<string> _sentencesParentheses = new List<string>();
+        public BracketString(string text)
+        {
+            _text = text;
+            _sentences = _text.Split(new char[] { '.', ',', '!', '?' });        // Масив який містить розділені рядки
+        }
+
+        public List<string> Check()                                             // Додає рядки які мають круглі дужки
+        {
+            foreach (var item in _sentences)
+            {
+                if (item.Contains("(") && item.Contains(")"))
+                {
+                    _sentencesParentheses.Add(item.Trim());
+                }
+            }
+            return _sentencesParentheses;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Console.WriteLine("Заданий текст: \n{0}", _text);
+            Console.WriteLine("\nРядки які містять круглі дужки: ");
+            foreach (var item in _sentencesParentheses)
+            {
+                stringBuilder.Append(item + " | ");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Home_task_4/Objective_1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Objective_1;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+BracketString bracket = new BracketString("Ось речення яке не містить дужок, але ось (тут), вони є." +
+                                            " Проте (тут,) воно не правельно, оскіль (там), кома в ()! Чому(?)");
+bracket.Check();
+Console.WriteLine(bracket.ToString());
