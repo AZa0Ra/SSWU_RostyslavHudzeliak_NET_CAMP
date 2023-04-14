e28526245a97004092a09f67fb670209ee41cba4
Rostyk <rostyk.gydzelyak@gmail.com>
2023-04-15 01:57:02 +0300

    Home_task_4-Objective_2

---
+++ Home_task_4/Objective_2/Email.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Objective_2
+{
+    internal class Email
+    {
+        private string[] _words;
+        List<string> correctEmails = new List<string>();
+        List<string> incorrectEmails = new List<string>();
+        public Email(string text)
+        {
+            _words = text.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        }
+        public bool CheckEmail(string email)        // Здійснює пошук за @ та .
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return false;
+            }
+            int atIndex = email.IndexOf('@');
+            if (atIndex == -1 || atIndex == 0 || atIndex == email.Length - 1 || email.IndexOf('@', atIndex + 1) != -1 || email.Length >= 64 || email.IndexOf('_', atIndex + 1) != -1)
+            {
+                return false;
+            }
+
+            int pointIndex = email.IndexOf('.', atIndex + 1);
+            if (pointIndex == -1 || pointIndex == atIndex + 1 || pointIndex == email.Length - 1)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void SortEmails()               // Переміщує адресу електронної пошти до колекції дійсних або недійсних адрес
+        {
+            foreach (var item in _words)
+            {
+                if (item.Contains('@'))
+                {
+                    if (CheckEmail(item))
+                    {
+                        correctEmails.Add(item);
+                    }
+                    else
+                    {
+                        incorrectEmails.Add(item);
+                    }
+                }
+                else
+                {
+                    incorrectEmails.Add(item);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder correctStringBuilder = new StringBuilder();
+            StringBuilder incorrectStringBuilder1 = new StringBuilder();
+            foreach (var item in correctEmails)
+            {
+                correctStringBuilder.Append(item.ToString() + '\n');
+            }
+
+            foreach (var item in incorrectEmails)
+            {
+                incorrectStringBuilder1.Append(item.ToString() + '\n');
+            }
+            return $"Правильнi електроннi адреси:\n" + correctStringBuilder.ToString()+ "\nНеправильнi електроннi адреси:\n" + incorrectStringBuilder1.ToString();
+        }
+    }
+}
---
+++ Home_task_4/Objective_2/Program.cs
@@ -0,0 +1,10 @@
+﻿using Objective_2;
+
+Email email = new Email("simple@example.com very.common@example.com disposable.style.email.with+symbol@example.com other.email-with-hyphen@example.com" +
+                " fully-qualified-domain@example.com user.name+tag+sorting@example.com x@example.com example-indeed@strange-example.com" +
+                " example@s.example \"john..doe\"@example.org mailhost!username@example.org user%example.com@example.org" +
+                " Abc.example.com A@b@c@example.com 1234567890123456789012345678901234567890123456789012345678901234+x@example.com" +
+                " i_like_underscore@but_its_not_allow_in_this_part.example.com a\"(c)d,e:f;g<h>i[j\\k]l@example.com");
+
+email.SortEmails();
+Console.WriteLine(email.ToString());
